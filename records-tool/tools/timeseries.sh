51468f06a1e59c32da16571395d13060a8886c37
James Ciarlo <39583616+ciarloj@users.noreply.github.com>
2022-02-07 18:07:07 +0100

    bug fixes

--- records-tool/tools/timeseries.sh
+++ records-tool/tools/timeseries.sh
@@ -2,6 +2,10 @@
 {
 set -eo pipefail
 
+CDO(){
+  cdo -O -L -f nc4 -z zip $@
+}
+
 ps=F # T or F : process skip
 nlist=$1
 v=$( cat $nlist | grep 'var =' | cut -d"'" -f2 )
@@ -11,21 +15,23 @@ dn=$( cat $nlist | grep 'd_nam =' | cut -d"'" -f2 )
 y1=$( cat $nlist | grep 'fyr =' | cut -d"'" -f2 )
 y2=$( cat $nlist | grep 'lyr =' | cut -d"'" -f2 )
 
+stat=mean
+
 hdir=$wkd/$jm/$dn
 ydir=$hdir
-mdir=$ydir/fldtimmean
+mdir=$ydir/fldtim$stat
 mkdir -p $mdir
 
 for y in $( seq $y1 $y2 ); do
   echo $y ..
   inf=$ydir/${y}.nc
   of=$mdir/${y}.nc
-  [[ $ps != T ]] && eval cdo -O -L -f nc4 -z zip timmean -fldmean $inf $of
+  [[ $ps != T ]] && eval CDO tim$stat -fld$stat $inf $of
 done
 
 infs=$mdir/{${y1}..${y2}}.nc
 mf=$mdir/merge.nc
-[[ $ps != T ]] && eval cdo -O -L -f nc4 -z zip mergetime $infs $mf
+[[ $ps != T ]] && eval CDO mergetime $infs $mf
 
 #idir=$wkd/$jm/images
 #mkdir -p $idir
